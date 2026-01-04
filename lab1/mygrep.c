a7201c91cf0c00b9b864309f9f69968955dab4ba
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-01-04 15:39:37 +0300

    mygrep реализован поиск подстроки в потоке и файлах

--- lab1/mygrep.c
+++ lab1/mygrep.c
@@ -1,7 +1,37 @@
-//?
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-    printf("mygrep is here!\n");
+void grep_stream(FILE *stream, const char *pattern) {
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), stream) != NULL) {
+        if (strstr(buffer, pattern) != NULL) {
+            fputs(buffer, stdout);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s pattern [file...]\n", argv[0]);
+        return 1;
+    }
+
+    const char *pattern = argv[1];
+    int file_start = 2;
+
+    if (argc == file_start) {
+        // Только stdin
+        grep_stream(stdin, pattern);
+    } else {
+        for (int i = file_start; i < argc; i++) {
+            FILE *fp = fopen(argv[i], "r");
+            if (fp == NULL) {
+                perror(argv[i]);
+            } else {
+                grep_stream(fp, pattern);
+                fclose(fp);
+            }
+        }
+    }
     return 0;
 }
