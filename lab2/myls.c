92e4798d1400e2c668b1cfe488732d65c5e21833
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-01-05 20:17:50 +0300

    myls.c базовая функциональность — вывод списка непустых имён файлов (без скрытых) в текущей директории

---
+++ lab2/myls.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>   // +
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    // По умолчанию директория "."
+    const char *path = ".";
+    
+    // Открываем директорию
+    DIR *dir = opendir(path);
+    if (!dir)
+    {
+        perror("opendir");
+        return 1;
+    }
+
+    struct dirent *entry;
+    
+    // Читаем по одной записи
+    while ((entry = readdir(dir)) != NULL) {
+        // Пропускаем скрытые файлы нач с .
+        if (entry->d_name[0] == '.')
+        {
+            continue;
+        }
+        printf("%s\n", entry->d_name);
+    }
+
+    closedir(dir);
+    return 0;
+}
