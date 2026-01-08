2c191398a925f2a6a91b5a3a7f5ec7da537d7bab
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-01-09 02:55:38 +0300

    убрал предупреждения и добав перевод строки в конце файла

--- lab4/mychmod.c
+++ lab4/mychmod.c
@@ -4,7 +4,6 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-// проверка, что строка сост из 0–7
 int is_octal_string(const char *s) {
     if (!s || !*s) return 0;
     for (int i = 0; s[i] != '\0'; i++) {
@@ -15,6 +14,20 @@ int is_octal_string(const char *s) {
     return 1;
 }
 
+void apply_octal_mode(const char *mode_str, int num_files, char **files) {
+    mode_t new_mode = (mode_t)strtol(mode_str, NULL, 8);
+    for (int i = 0; i < num_files; i++) {
+        if (chmod(files[i], new_mode) == -1) {
+            perror(files[i]);
+        }
+    }
+}
+
+void apply_symbolic_mode(const char *mode_str) {
+    fprintf(stderr, "ошибка: симв режим ('%s') не подд\n", mode_str);
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         fprintf(stderr, "использование: %s <режим> <файл>...\n", argv[0]);
@@ -22,21 +35,13 @@ int main(int argc, char *argv[]) {
     }
 
     char *mode_str = argv[1];
+    int num_files = argc - 2;
+    char **files = argv + 2;
 
-    // ТОЛЬКО октальный режим (сейчас)
-    if (!is_octal_string(mode_str)) {
-        fprintf(stderr, "ошибка: только окт_реж\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // превр стр в число по основанию 8
-    mode_t new_mode = (mode_t)strtol(mode_str, NULL, 8);
-
-    // новые права ко всем файлам
-    for (int i = 2; i < argc; i++) {
-        if (chmod(argv[i], new_mode) == -1) {
-            perror(argv[i]);
-        }
+    if (is_octal_string(mode_str)) {
+        apply_octal_mode(mode_str, num_files, files);
+    } else {
+        apply_symbolic_mode(mode_str);
     }
 
     return 0;
