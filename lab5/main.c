175f98f1458c1edec39c94b9647d8ac31b5c0aa1
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-01-10 23:54:14 +0300

    реализовал флаг -s для просмотра содержимого архива

--- lab5/main.c
+++ lab5/main.c
@@ -27,14 +27,12 @@ void print_help() {
 }
 
 int add_file_to_archive(const char *archive_name, const char *filename) {
-    // 1. Открываем входной файл для чтения
     int file_fd = open(filename, O_RDONLY);
     if (file_fd == -1) {
         perror("Ошибка открытия входного файла");
         return -1;
     }
 
-    // 2. Получаем информацию о файле
     struct stat st;
     if (fstat(file_fd, &st) == -1) {
         perror("Ошибка fstat");
@@ -42,7 +40,6 @@ int add_file_to_archive(const char *archive_name, const char *filename) {
         return -1;
     }
 
-    // 3. Открываем архив в режиме дозаписи (если нет — создаём)
     int archive_fd = open(archive_name, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (archive_fd == -1) {
         perror("Ошибка открытия архива");
@@ -50,18 +47,16 @@ int add_file_to_archive(const char *archive_name, const char *filename) {
         return -1;
     }
 
-    // 4. Заполняем заголовок
     FileHeader header;
     memset(&header, 0, sizeof(header));
     strncpy(header.magic, HEADER_MAGIC, 8);
-    strncpy(header.filename, filename, 15); // максимум 15 символов
+    strncpy(header.filename, filename, 15);
     header.original_size = st.st_size;
     header.mode = st.st_mode;
     header.uid = st.st_uid;
     header.gid = st.st_gid;
     header.mtime = st.st_mtime;
 
-    // 5. Пишем заголовок в архив
     if (write(archive_fd, &header, sizeof(header)) != (ssize_t)sizeof(header)) {
         perror("Ошибка записи заголовка");
         close(file_fd);
@@ -69,7 +64,6 @@ int add_file_to_archive(const char *archive_name, const char *filename) {
         return -1;
     }
 
-    // 6. Копируем содержимое файла блоками
     char buffer[4096];
     ssize_t nread;
     while ((nread = read(file_fd, buffer, sizeof(buffer))) > 0) {
@@ -88,11 +82,59 @@ int add_file_to_archive(const char *archive_name, const char *filename) {
         return -1;
     }
 
-    // 7. Закрываем файлы
     close(file_fd);
     close(archive_fd);
 
     printf("Файл '%s' успешно добавлен в архив '%s'\n", filename, archive_name);
+    return 0;
+}
+
+int show_archive_stat(const char *archive_name) {
+    int archive_fd = open(archive_name, O_RDONLY);
+    if (archive_fd == -1) {
+        perror("Ошибка открытия архива");
+        return -1;
+    }
+
+    FileHeader header;
+    off_t offset = 0;
+
+    printf("Archive contents:\n");
+    printf("%-20s %-10s %-10s %-10s\n", "Filename", "Size", "UID", "GID");
+
+    while (1) {
+        if (lseek(archive_fd, offset, SEEK_SET) == -1) {
+            perror("Ошибка lseek");
+            close(archive_fd);
+            return -1;
+        }
+
+        ssize_t nread = read(archive_fd, &header, sizeof(header));
+        if (nread == 0) {
+            break; // конец файла
+        }
+        if (nread != sizeof(header)) {
+            fprintf(stderr, "Ошибка: повреждённый заголовок на позиции %lld\n", offset);
+            close(archive_fd);
+            return -1;
+        }
+
+        if (strncmp(header.magic, HEADER_MAGIC, 8) != 0) {
+            fprintf(stderr, "Ошибка: неверная сигнатура на позиции %lld\n", offset);
+            close(archive_fd);
+            return -1;
+        }
+
+        printf("%-20s %-10ld %-10d %-10d\n",
+               header.filename,
+               (long)header.original_size,
+               header.uid,
+               header.gid);
+
+        offset += sizeof(header) + header.original_size;
+    }
+
+    close(archive_fd);
     return 0;
 }
 
@@ -107,7 +149,7 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    if (argc < 4) {
+    if (argc < 3) {
         fprintf(stderr, "Не хватает аргументов\n");
         print_help();
         return 1;
@@ -115,6 +157,24 @@ int main(int argc, char *argv[]) {
 
     char *archive = argv[1];
     char *flag = argv[2];
+
+    // Обработка флага -s (не требует третьего аргумента)
+    if (strcmp(flag, "-s") == 0 || strcmp(flag, "--stat") == 0) {
+        if (argc != 3) {
+            fprintf(stderr, "Для -s не нужен дополнительный аргумент\n");
+            print_help();
+            return 1;
+        }
+        return show_archive_stat(archive);
+    }
+
+    // Все остальные флаги требуют третьего аргумента
+    if (argc < 4) {
+        fprintf(stderr, "Не хватает аргументов для '%s'\n", flag);
+        print_help();
+        return 1;
+    }
+
     char *file = argv[3];
 
     if (strcmp(flag, "-i") == 0 || strcmp(flag, "--input") == 0) {
