197e31427622fd04401766e719c5ea39ee6c94a3
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-01-11 13:41:33 +0300

    реализована функция пишущего потока с записью монотонного счётчика

--- lab10/main.c
+++ lab10/main.c
@@ -10,3 +10,21 @@ int shared_array[ARRAY_SIZE];
 int write_counter = 0;
 
 pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
+
+void* writer(void* arg) {
+    while (1) {
+        pthread_rwlock_wrlock(&rwlock);
+
+        write_counter++;
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            shared_array[i] = write_counter;
+        }
+
+        printf("Writer (tid=%lu): wrote %d to array\n", pthread_self(), write_counter);
+
+        pthread_rwlock_unlock(&rwlock);
+
+        sleep(1);
+    }
+    return NULL;
+}
