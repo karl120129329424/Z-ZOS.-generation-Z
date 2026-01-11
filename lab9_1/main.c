cadd38857229905c07fdcb5e0c87e3f7a153e80e
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-01-11 05:17:30 +0300

    заменил deprecated sem_init/sem_destroy на pthread_mutex для совместимости с macOS

--- lab9_1/main.c
+++ lab9_1/main.c
@@ -5,12 +5,20 @@
 #include <string.h>
 
 #define ARRAY_SIZE 10
-char shared_array[ARRAY_SIZE] = "init";
+#define MAX_ITERATIONS 5
+
+// Общий буфер и мьютекс
+char shared_array[ARRAY_SIZE];
+pthread_mutex_t array_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* writer_thread(void* arg) {
+    (void)arg; // подавляем предупреждение
     int counter = 0;
-    while (counter < 5) {
+    while (counter < MAX_ITERATIONS) {
+        pthread_mutex_lock(&array_mutex);
         snprintf(shared_array, ARRAY_SIZE, "%d", counter);
+        pthread_mutex_unlock(&array_mutex);
+
         printf("Писатель записал: %s\n", shared_array);
         usleep(1000000); // 1 секунда
         counter++;
@@ -19,10 +27,16 @@ void* writer_thread(void* arg) {
 }
 
 void* reader_thread(void* arg) {
+    (void)arg; // подавляем предупреждение
     pthread_t tid = pthread_self();
     int counter = 0;
-    while (counter < 5) {
-        printf("Читатель [TID: %lu] прочитал: %s\n", tid, shared_array);
+    while (counter < MAX_ITERATIONS) {
+        pthread_mutex_lock(&array_mutex);
+        char local_copy[ARRAY_SIZE];
+        strncpy(local_copy, shared_array, ARRAY_SIZE);
+        pthread_mutex_unlock(&array_mutex);
+
+        printf("Читатель [TID: %p] прочитал: %s\n", (void*)tid, local_copy);
         usleep(500000); // 0.5 секунды
         counter++;
     }
