2fceff1a6559846fbc0e5855c7491ab27206f2a6
kirillbelyaitse <kirillbelyaitsev@yandex.com>
2026-01-11 13:37:21 +0300

    скелет лабы +

---
+++ lab10/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define NUM_READERS 10
+#define ARRAY_SIZE 5
+
+int shared_array[ARRAY_SIZE];
+int write_counter = 0;
